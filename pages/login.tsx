1981c9d0ad60c694bbe316a52feac6a5c5e80bf4
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-09-12 16:03:32 -0500

    added test tailwindui component

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import styles from '../styles/Home.module.css';
+import Store from './components/StoreUI';
 
 const login = () => {
   return (
     <div className={styles.container}>
+      <Store/>
       <h1>Connect your wallet to continue.</h1>
       <p>Note: only using testnet until further notice</p>
       <button>sign in with metamask</button>
