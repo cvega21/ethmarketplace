515b23230bb01c4bdd4204b7eaf700bdf8a7a87e
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-09-21 00:03:24 -0500

    changed list page to night mode and mobile first

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -5,26 +5,18 @@ import { faGgCircle } from '@fortawesome/free-brands-svg-icons'
 
 const NavBar = () => {
   return (
-        <nav className="bg-gray-800 text-white p-5 m-0 w-screen flex justify-between">
-          <div>
+        <nav className="bg-indigo-800 pt-3 pb-3 text-white m-0 w-full flex justify-between items-center">
+          <div className='flex'>
             <Link href="/" passHref={true}>
-              <a className="font-bold ml-10 text-4xl font-dramatic flex items-center">
+              <a className="font-bold ml-10 text-3xl font-dramatic flex items-center">
                 <FontAwesomeIcon icon={faGgCircle}/> 
-                <h1 className="ml-3 mt-2 text-5xl">FIRE ON CHAIN</h1>
+                <h1 className="ml-3 mt-2 text-4xl">FIRE ON CHAIN</h1>
               </a>
             </Link>
-          </div>
-          <div className="flex mr-20 items-center">
-            {/* <a className="font-medium mr-12 text-xl" href="buy">
-              Buy
-            </a>
-            <a className="font-medium ml-12 text-xl">
-              Sell
-            </a> */}
-          </div>
-          <div className="flex mr-20 items-center">
-            <button className="bg-indigo-600 hover:bg-indigo-800 text-white font-bold py-2 px-4 border border-indigo-700 rounded">
-             Connect Wallet
+          </div> 
+          <div className="flex mr-10 mb-1 items-center">
+            <button className="text-4xl text-white borderrounded">
+            ☰
             </button>
           </div>
         </nav>
