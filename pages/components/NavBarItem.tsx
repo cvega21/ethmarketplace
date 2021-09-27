00d15b0633a3357b64a52343f33e3798ad284e4e
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-09-26 19:15:31 -0500

    created navbaritem component

---
+++ pages/components/NavBarItem.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const NavBarItem = (props: any) => {
+  return (
+    <a href='#' className='flex w-full text-gray-50 text-2xl font-light pl-6 h-16 items-center hover:bg-indigo-500 hover:cursor-pointer hover:text-white transition duration-150'>
+      <div className='w-12'>
+        <FontAwesomeIcon icon={props.icon}/> 
+      </div>
+      <h2 className='pl-4'>{props.title}</h2>
+    </a>
+  )
+}
+
+export default NavBarItem
