54ce624ca6d788409481ddff63f23c81b370713c
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-09-27 21:15:53 -0500

    modified navbaritem formatting

--- pages/components/NavBarItem.tsx
+++ pages/components/NavBarItem.tsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const NavBarItem = (props: any) => {
   return (
     <a href={props.title} className='flex w-full text-gray-50 text-2xl font-light pl-6 h-16 items-center hover:bg-indigo-500 hover:cursor-pointer hover:text-white transition duration-150'>
-      <div className='w-12'>
+      <div className='w-10 text-xl'>
         <FontAwesomeIcon icon={props.icon}/> 
       </div>
       <h2 className='pl-4'>{props.title}</h2>
