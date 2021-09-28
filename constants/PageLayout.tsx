c99e26e91883cf5f79b9de446b0bfa03655cb745
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-09-28 00:50:50 -0500

    extracted page layout component

--- constants/PageLayout.tsx
+++ constants/PageLayout.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import AppContext from '../contexts/AppContext'
 
 const PageLayout = () => {
+  const navContext = useContext(AppContext);
+  
   return (
-    <div>
+    <div className={`text-center dark flex flex-1 flex-col items-center bg-gray-900 relative ${navContext?.navIsOpen ? 'min-h-screen overflow-hidden' : 'min-h-screen'}`}>
       
     </div>
   )
