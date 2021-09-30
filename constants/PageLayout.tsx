823878f53f9022d731e2791c895770133fa612c3
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-09-29 23:02:11 -0500

    added title to buy page

--- constants/PageLayout.tsx
+++ constants/PageLayout.tsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
-import { useAppContext } from '../contexts/AppContext'
+import { useAppContext } from '../contexts/AppContext';
+import NavBar from '../components/NavBar';
 
 const PageLayout: React.FC = ({ children }) => {
   const navContext = useAppContext();
   
   return (
-    <div className={`text-center dark flex flex-1 flex-col items-center bg-gray-900 relative ${navContext?.navIsOpen ? 'min-h-screen overflow-hidden' : 'min-h-screen'}`}>
+    <div className={`text-center dark flex flex-1 flex-col items-center bg-gray-900 relative ${navContext?.navIsOpen ? 'max-h-screen overflow-hidden' : 'min-h-screen'}`}>
+      <NavBar/>
       {children}
     </div>
   )
