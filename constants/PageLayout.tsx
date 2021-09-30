cffb7724adcc387d513594e71e3fb048b109cf7e
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-09-29 22:54:58 -0500

    created basic app layout component

--- constants/PageLayout.tsx
+++ constants/PageLayout.tsx
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react'
-import AppContext from '../contexts/AppContext'
+import React, { useContext } from 'react';
+import { useAppContext } from '../contexts/AppContext'
 
-const PageLayout = () => {
-  const navContext = useContext(AppContext);
+const PageLayout: React.FC = ({ children }) => {
+  const navContext = useAppContext();
   
   return (
     <div className={`text-center dark flex flex-1 flex-col items-center bg-gray-900 relative ${navContext?.navIsOpen ? 'min-h-screen overflow-hidden' : 'min-h-screen'}`}>
-      
+      {children}
     </div>
   )
 }
