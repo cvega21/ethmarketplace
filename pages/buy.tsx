15b517721dd34915ca6262ad6bbb0d68cff93164
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-09-29 22:55:10 -0500

    added page layout to buy page

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Product from "../components/Product";
+import PageLayout from '../constants/PageLayout';
+import NavBar from '../components/NavBar';
 
 const buy = () => {
   return (
-    <div className="flex flex-col w-11/12 mt-12">
-    <h1 className="font-light justify-self-start text-left text-gray-50 text-2xl ml-4">top picks near you</h1>
-    <div className="flex flex-col">
-      <Product
+  <PageLayout>
+      <NavBar/>
+      <h1 className="font-light justify-self-start text-left text-gray-50 text-2xl ml-4">top picks near you</h1>
+      <div className="flex flex-col">
+        <Product
         image="coffee_table.jpeg"
         title="modern coffee table"
         price={0.000042}
@@ -14,7 +17,7 @@ const buy = () => {
         location="austin, tx"
         uid={123}
         />
-      <Product
+       <Product
         image="iphone.jpeg"
         title="iphone 12"
         price={0.000095}
@@ -22,16 +25,16 @@ const buy = () => {
         location="san marcos, tx"
         uid={123}
         />
-      <Product
+       <Product
         image="jordans.jpeg"
         title="jordan 1's"
         price={0.00004}
         action={'buy'}
         location="round rock, tx"
         uid={123}
-      />
-    </div>
-  </div>
+        />
+      </div>
+  </PageLayout>
   )
 }
 
