4a67414c9fbfa2d012761ea7274cefb6da3f415d
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-09-30 23:54:38 -0500

    changed tags from a->Link in navbaritem

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -13,13 +13,13 @@ const ActionButton = (props: IProps) => {
   return (
     props.theme === 'light' ?
       <Link href={`/${props.link}`} passHref={true}>
-          <button className="bg-indigo-200 hover:bg-indigo-100 text-indigo-800 hover:text-indigo-700 font-medium text-xl py-2 px-8 my-4 rounded-lg shadow-indigo w-full">
+          <button className="bg-indigo-200 hover:bg-indigo-100 text-indigo-800 hover:text-indigo-900 font-medium text-xl py-2 px-8 my-4 rounded-lg shadow-indigo w-full transition-all duration-200 ease-in-out">
             {props.children}
           </button>
       </Link>
     : props.theme === 'dark' ?
       <Link href={`/${props.link}`} passHref={true}>
-          <button className="bg-indigo-700 rounded-lg hover:bg-indigo-800 text-gray-100 hover:text-white font-medium text-xl py-2 px-8 my-4 shadow-indigo w-full">
+          <button className="bg-indigo-700 rounded-lg hover:bg-indigo-800 text-gray-100 hover:text-white font-medium text-xl py-2 px-8 my-4 shadow-indigo w-full transition-all duration-200 ease-in-out">
             {props.children}
           </button>
       </Link>
--- components/NavBarItem.tsx
+++ components/NavBarItem.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
+import { useAppContext } from '../contexts/AppContext'
 
 const NavBarItem = (props: any) => {
+  const navContext = useAppContext();
+  
   return (
-    <a href={props.title} className='flex w-full text-gray-50 text-2xl font-light pl-6 h-16 items-center hover:bg-indigo-500 hover:cursor-pointer hover:text-white transition duration-150'>
-      <div className='w-10 text-xl'>
-        <FontAwesomeIcon icon={props.icon}/> 
-      </div>
-      <h2 className='pl-4'>{props.title}</h2>
-    </a>
+    <Link href={props.title} passHref={true}>
+      <a className='flex w-full text-gray-50 text-2xl font-light pl-6 h-16 items-center hover:bg-indigo-500 hover:cursor-pointer hover:text-white transition duration-150' onClick={() => navContext?.setNavIsOpen(!navContext.navIsOpen)}>
+        <div className='w-10 text-xl'>
+          <FontAwesomeIcon icon={props.icon}/> 
+        </div>
+        <h2 className='pl-4'>{props.title}</h2>
+      </a>
+    </Link>
   )
 }
 
