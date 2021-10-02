4b5c4de12b8013345587e7a490c6df0e41d5dd9c
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-10-01 22:59:41 -0500

    added navbar for web

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect, useContext } from 'react'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFireAlt, faStore, faCoins, faGraduationCap, faUserCircle } from '@fortawesome/free-solid-svg-icons'
-import NavBarItem from './NavBarItem'
+import NavBarItemMobile from './NavBarItemMobile';
+import NavBarItem from './NavBarItem';
 import { useAppContext } from '../contexts/AppContext'
 
 const NavBar = () => {
@@ -10,7 +11,7 @@ const NavBar = () => {
   
   return (
       <>
-        <nav className="bg-gray-700 bg-opacity-50 text-white w-full z-20">
+        <nav className="bg-gray-700 bg-opacity-50 text-white w-full z-20 relative">
           <div className='mobile-top-bar flex justify-between relative drop-shadow-2xl'>
             <div className='flex justify-between w-full'>
               <Link href="/" passHref={true}>
@@ -19,19 +20,25 @@ const NavBar = () => {
                   <h1 className="ml-2 text-4xl">firechain</h1>
                 </a>
               </Link>
-              <button className="text-4xl text-gray-50 mr-8" onClick={e => {
+              <button className="text-4xl text-gray-50 mr-8m transform-gpu transition-all duration-200 ease-in-out md:-translate-y-full absolute md:static right-8 top-3" onClick={e => {
                 navContext?.setNavIsOpen(!navContext?.navIsOpen);
               }}>
               {navContext?.navIsOpen ? 'x' : '☰'}
               </button>
+              <ul className='flex text-xl transform-gpu transition-all duration-200 ease-in-out -translate-y-full md:translate-y-0 md:absolute md:right-0 h-full'>
+                <NavBarItem title='buy'/>
+                <NavBarItem title='sell'/>
+                <NavBarItem title='learn'/>
+                <NavBarItem title='account'/>
+              </ul>
             </div>
           </div> 
         </nav>
         <div className={`bg-gray-800 mobile-side-bar w-full absolute inset-y-0 top-16 left-0 transform-gpu transition duration-200 ease-in-out z-10 ${!navContext?.navIsOpen ? '-translate-x-full' : ''}`}>
-          <NavBarItem icon={faStore} title='buy'/>
-          <NavBarItem icon={faCoins} title='sell'/>
-          <NavBarItem icon={faGraduationCap} title='learn'/>
-          <NavBarItem icon={faUserCircle} title='account'/>
+          <NavBarItemMobile icon={faStore} title='buy'/>
+          <NavBarItemMobile icon={faCoins} title='sell'/>
+          <NavBarItemMobile icon={faGraduationCap} title='learn'/>
+          <NavBarItemMobile icon={faUserCircle} title='account'/>
         </div>
       </>
   )
--- components/NavBarItem.tsx
+++ components/NavBarItem.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
-import { useAppContext } from '../contexts/AppContext'
 
-const NavBarItem = (props: any) => {
-  const navContext = useAppContext();
-  
+interface INavBarItem {
+  title: string
+}
+
+const NavBarItem = (props: any ) => {
   return (
     <Link href={props.title} passHref={true}>
-      <a className='flex w-full text-gray-50 text-2xl font-light pl-6 h-16 items-center hover:bg-indigo-500 hover:cursor-pointer hover:text-white transition duration-150' onClick={() => navContext?.setNavIsOpen(!navContext.navIsOpen)}>
-        <div className='w-10 text-xl'>
-          <FontAwesomeIcon icon={props.icon}/> 
-        </div>
-        <h2 className='pl-4'>{props.title}</h2>
+      <a className='px-10 hover:border-indigo-400 hover:text-indigo-400 p-5 border-b-2 border-opacity-0 h-16 transition duration-200 ease-in-out text-gray-300 font-light'>
+        {props.title}
       </a>
     </Link>
   )
