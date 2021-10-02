25224a0c2a4d3bd1ffce1e02db3979f4fca51c9b
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-10-01 22:59:20 -0500

    added mobile navbar item component

---
+++ components/NavBarItemMobile.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
+import { useAppContext } from '../contexts/AppContext'
+
+const NavBarItemMobile = (props: any) => {
+  const navContext = useAppContext();
+  
+  return (
+    <Link href={props.title} passHref={true}>
+      <a className='flex w-full text-gray-50 text-2xl font-light pl-6 h-16 items-center hover:bg-indigo-500 hover:cursor-pointer hover:text-white transition duration-150' onClick={() => navContext?.setNavIsOpen(!navContext.navIsOpen)}>
+        <div className='w-10 text-xl'>
+          <FontAwesomeIcon icon={props.icon}/> 
+        </div>
+        <h2 className='pl-4'>{props.title}</h2>
+      </a>
+    </Link>
+  )
+}
+
+export default NavBarItemMobile
