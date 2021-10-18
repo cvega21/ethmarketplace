e75894f4f0fd534b1a8fa6e13811b254e8376331
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-10-17 23:34:45 -0500

    added product description section

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -9,6 +9,8 @@ import { IProduct } from '../../types/types'
 import styles from '../../styles/Product.module.css'
 import ActionButton from '../../components/ActionButton'
 import EthPrice from '../../components/EthPrice'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faList, faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 
 interface IProps {
   product: IProduct
@@ -37,28 +39,19 @@ const ProductPage = ({ product }: IProps) => {
             </div>
           </div>
           <div className='flex flex-col items-center'>
-            {/* <div className='w-full flex flex-col items-center justify-around'>
-              <div className='flex items-center'>
-                <h1 className='text-gray-300 font-extralight text-xl'>current price: </h1>
-                <div className='flex'>
-                  <Image src="/eth.svg" height={30} width={30} alt="ethereum" />
-                  <h1 className='text-white text-3xl font-bold ml-1'>0.00123</h1>
-                </div>
-              </div>
-            </div> */}
             <div className='w-10/12 flex items-start'>
               <h1 className='text-gray-300 font-extralight text-xl'>listed by </h1>
               <a className='text-indigo-400 font-extralight text-xl ml-1'>@person12341</a>
             </div>
-            <div className='w-full flex items-center justify-around mt-12'>
+            <div className='w-full flex items-center justify-around mt-2'>
               <div className='w-10/12'>
                 <ActionButton theme='light'>
                   <div className='flex w-full items-center justify-between h-9'>
-                    <p className='text-2xl w-24'>bid</p>
+                    <p className='text-2xl font-light'>place bid</p>
                     <div className='flex'>
                       <Image src="/eth.svg" height={28} width={28} alt="ethereum" />
                       <div className='flex items-center'>
-                        <h1 className='text-indigo-800 text-xl font-normal ml-1'>0.0045</h1>  
+                        <h1 className='text-indigo-800 text-xl font-normal ml-1'>{product.startingPrice}</h1>  
                       </div>
                     </div>
                   </div>
@@ -69,33 +62,54 @@ const ProductPage = ({ product }: IProps) => {
               <div className='w-10/12'>
                 <ActionButton theme='dark'>
                   <div className='flex w-full items-center justify-between h-9'>
-                    <p className='text-2xl w-24'>buy now</p>
+                    <p className='text-2xl font-light w-24'>buy now</p>
                     <div className='flex'>
                       <Image src="/eth.svg" height={28} width={28} alt="ethereum" />
                       <div className='flex items-center'>
-                        <h1 className='text-xl font-normal ml-1'>0.0045</h1>  
+                        <h1 className='text-xl font-normal ml-1'>{product.buyNowPrice}</h1>  
                       </div>
                     </div>
                   </div>
                 </ActionButton>
               </div>
             </div>
-            {/* <div className='w-full flex border items-center justify-around'>
-              <div className='w-10/12'>
-                <ActionButton theme='dark'>
-                  <div className='flex w-full items-center justify-between'>
-                    <p className='text-2xl'>buy now</p>
-                    <div>
-                      <div className='flex w-32 h-10 pb-1 items-center border-b-2 border-indigo-500 justify-center'>
-                        <Image src="/eth.svg" height={30} width={30} alt="ethereum" />
-                        <h1 className='text-xl font-normal ml-1'>0.0031</h1>  
-                      </div>
-                      <p className='text-sm font-extralight ml-4 text-gray-300 mt-1'>$15,673</p>
-                    </div>
-                  </div>
-                </ActionButton>
+            <div className='text-black w-10/12 flex items-center justify-start border-t border-gray-700 mt-6'>
+              {/* <div className='px-3 bg-gray-100 border'> */}
+                {/* <div className='flex w-full items-center justify-start h-10 text-base'> */}
+                  {/* <FontAwesomeIcon icon={faQuoteLeft} className="mr-3"/>  */}
+                  {/* <p className='font-medium'>product details</p> */}
+                {/* </div> */}
+              {/* </div> */}
+            </div>
+            <div className='flex justify-start w-10/12 my-4'>
+              <h2 className='text-white font-normal text-xl'>product details</h2>
+            </div>
+            <div className='text-white w-10/12'>
+              <div className='flex justify-between'>
+                <h2 className='text-gray-200 font-light'>listed since</h2>
+                <h1 className='text-gray-400 font-extralight'>Aug. 13, 2021</h1>
               </div>
-            </div> */}
+              <div className='flex justify-between'>
+                <h2 className='text-gray-200 font-light'>condition</h2>
+                <h1 className='text-gray-400 font-extralight'>like new</h1>
+              </div>
+              <div className='flex justify-between'>
+                <h2 className='text-gray-200 font-light'>delivery options</h2>
+                <h1 className='text-gray-400 font-extralight'>pick up only</h1>
+              </div>
+              <div className='flex justify-between'>
+                <h2 className='text-gray-200 font-light'>contract address</h2>
+                <h1 className='text-indigo-400 font-extralight'>0x2j1ido..21ka</h1>
+              </div>
+              {/* <div className='flex justify-between'>
+                <h2 className='text-gray-200 font-light'>Token (NFT) ID</h2>
+                <h1 className='text-indigo-400 font-extralight'>1212401152...</h1>
+              </div> */}
+              <div className='flex flex-col items-start'>
+                <h2 className='text-gray-200 font-medium'>product description</h2>
+                <h1 className='text-gray-400 font-extralight text-left'>{product.description}</h1>
+              </div>
+            </div>
           </div>
         </div>
       </div>
