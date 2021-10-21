82ad5e2c305548ed055fc39bdae1285f155b227f
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-10-20 23:06:31 -0500

    added test, circleci config

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,13 @@ module.exports = async function main (callback) {
   try {
     // Our code will go here
     // Retrieve accounts from the local node
-    const accounts = await web3.eth.getAccounts();
-    console.log(accounts)
-    callback(0);
+    // const accounts = await web3.eth.getAccounts();
+    // console.log(accounts)
+    // callback(0);
+    const Box = artifacts.require('Box');
+    const box = await Box.deployed();
+    const value = await box.retrieve();
+    console.log('Box value is', value.toString());
   } catch (error) {
     console.error(error);
     callback(1);
