101bde61b6d628293a62010dd8a1301dbe0c5107
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-10-28 00:38:03 -0500

    prettified account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -18,14 +18,12 @@ const providerOptions = {
 const Account = () => {
   const appContext = useAppContext();
 
-  const [account, setAccount] = useState('');
+  useEffect(() => {
+    window.ethereum.selectedAddresses ? appContext?.connectMetamask() : void(0);
 
-  const connectMetamask = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
-    const account = accounts[0];
-    setAccount(account);
-  }
-
+    return () => {
+    }
+  }, [appContext])
 
   
 
@@ -33,21 +31,25 @@ const Account = () => {
     <PageLayout>
       <div className="text-center flex w-full flex-col lg:flex-1 justify-center items-center h-full">
         <div className="flex flex-col w-6/12 min-w-min md:w-3/12 lg:w-2/12">
-          <div className='rounded-full relative group cursor-pointer'>
-            <Image 
-              src='/avi_placeholder.png' 
-              height='200' 
-              width='200' 
-              alt='avatar placeholder'
-              className='rounded-full group-hover:opacity-60'
-            />
-          <FontAwesomeIcon 
-            icon={faEdit}
-            className='absolute left-1/2 top-1/2 text-white text-3xl opacity-0 group-hover:opacity-100'
-          />
-          <h1 className='text-gray-100 font-medium text-5xl my-2'>Vitalik Buterin</h1>
-          <h2 className='text-indigo-400 font-thin text-4xl'>@vbuterin</h2>
-          <h1 className='text-gray-400 font-light text-xl my-2'>{appContext?.account}</h1>
+          <div className='flex w-6/12 min-w-full'>
+            <div className='rounded-full relative group cursor-pointer w-60 flex'>
+              <Image 
+                src='/avi_placeholder.png' 
+                height='200' 
+                width='200' 
+                alt='avatar placeholder'
+                className='rounded-full group-hover:opacity-60 min-w-full min-h-full'
+              />
+              <FontAwesomeIcon 
+                icon={faEdit}
+                className='absolute left-1/2 top-1/2 text-white text-3xl opacity-0 group-hover:opacity-100 transform -translate-x-2 -translate-y-2'
+              />
+            </div>
+            <div className='flex flex-col justify-center'>
+              <h1 className='text-gray-100 font-bold text-5xl my-2 text-left'>Vitalik Buterin</h1>
+              <h2 className='text-indigo-400 font-thin text-4xl text-left'>@vbuterin</h2>
+              <h1 className='text-gray-500 font-light text-xl my-2'>{appContext?.account}</h1>
+            </div>
           </div>
           
           {appContext?.account ? 
@@ -55,14 +57,13 @@ const Account = () => {
             {/* <p>{appContext?.account}</p> */}
           </div>
           :
-          <></>
-          }
           <button 
             className='bg-indigo-700 rounded-lg hover:bg-indigo-800 text-gray-100 hover:text-white font-medium text-xl py-2 px-8 my-4 shadow-indigo w-full transition-all duration-200 ease-in-out'
             onClick={() => appContext?.connectMetamask()}
             >
               connect to metamask
           </button>
+          }
         </div>
       </div>
     </PageLayout>
