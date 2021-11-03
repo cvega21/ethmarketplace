67fb5fbcc422e53252bc6f7517aa90b84a3d4c16
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-11-02 22:12:39 -0500

    add IPFS function [skip ci]

---
+++ utils/pinJSONToIPFS.ts
@@ -0,0 +1,34 @@
+// require('dotenv').config();
+const key = process.env.NEXT_PUBLIC_PINATA_KEY;
+const secret = process.env.NEXT_PUBLIC_PINATA_SECRET;
+
+import axios from 'axios'
+
+const pinJSONToIPFS = async(JSONBody: any) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+
+    console.log(key, secret);
+
+    try {
+      const response = await axios.post(url, JSONBody, {
+        headers: {
+            pinata_api_key: key as string,
+            pinata_secret_api_key: secret as string,
+        }
+      });
+      return {
+        success: true,
+        pinataUrl: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash,
+        message: 'Congrats! You just minted an NFT!'
+      }
+
+    } catch (e) {
+      return {
+        success: false,
+        pinataUrl: '',
+        message: e.message
+      }
+    }
+};
+
+export default pinJSONToIPFS
