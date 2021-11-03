c5ae01488d591a50f0dbbb5b9ea17f1c2ea5751b
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-11-02 20:00:00 -0600

    added IPFS implementation to metadata endpoint

--- pages/api/metadata/[id].ts
+++ pages/api/metadata/[id].ts
@@ -1,27 +1,59 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { db } from '../../../constants/firebase'
 import { collection, doc, getDoc, getDocs, limit, onSnapshot, query, where } from '@firebase/firestore';
-import { IProduct } from '../../../types/types'
+import { IProduct } from '../../../types/types';
+import pinJSONToIPFS from '../../../utils/pinJSONToIPFS';
 
 
-interface INFTMetadataRes {
+interface INFTMetadata {
   title: string,
   description: string,
   imagePath: string
 }
 
-const getMetadata = async (req: NextApiRequest, res: NextApiResponse<INFTMetadataRes>) => {
-  const refString = req.query.id as string;
-  const productRef = doc(db, 'products', refString);
-  const product = await getDoc(productRef);
-  const productData: IProduct = product.data() as IProduct;
-  const {title, description, imagePath} = productData;
-
-  res.status(200).json({
-    title: title,
-    description: description,
-    imagePath: imagePath
-  });
+interface IPinataRes {
+  tokenURI: string
 }
 
-export default getMetadata
+interface IErrorRes {
+  error: string
+}
+
+export default async function getMetadata(req: NextApiRequest, res: NextApiResponse<INFTMetadata|IPinataRes|any>) {
+  
+  if (req.method === 'POST') {
+    // IPFS (Decentralized) Implementation
+    console.log(req.body)
+    const NFTMetadata: INFTMetadata = req.body;
+    const pinataResponse = await pinJSONToIPFS(NFTMetadata);
+
+    if (!pinataResponse.success) {
+      res.status(400).json({error: 'error calling pinata.'});
+    } else {
+      res.status(200).json({
+        tokenURI: pinataResponse.pinataUrl
+      });
+    }
+  } else {
+    // Firebase (Centralized) Implementation
+    try {
+      const refString = req.query.id as string;
+      const productRef = doc(db, 'products', refString);
+      const product = await getDoc(productRef);
+      const productData: IProduct = product.data() as IProduct;
+      const {title, description, imagePath} = productData;
+    
+      res.status(200).json({
+        title: title,
+        description: description,
+        imagePath: imagePath
+      });
+    } catch (e) {
+      res.status(400).json({
+        error: 'error in the firebase call.' 
+      })
+    }
+  }
+}
+
+// export default getMetadata
