694d614dae019c9c5b1a43a0d58541d9a76b52a8
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-11-13 16:05:00 -0600

    added minting function at .0001 eth

--- contracts/Firechain.sol
+++ contracts/Firechain.sol
@@ -1,10 +1,25 @@
-// contracts/MyNFT.sol
+//Contract based on https://docs.openzeppelin.com/contracts/3.x/erc721
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract FireChainNFT is ERC721 {
-    constructor() ERC721("FireChainNFT", "MNFT") {
-    } 
+contract Firechain is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Firechain", "FIRE") {}
+
+    function mintNFT(address recipient, string memory tokenURI) public payable returns (uint256) {
+        _tokenIds.increment();
+
+        require(msg.value >= 100000000000000, "Not enough ETH sent. Minting price is 0.0001!");
+
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
 }
