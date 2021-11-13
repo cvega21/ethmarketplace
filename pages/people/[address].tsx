5000d661f8be3c2404a581e78a87522bc014f077
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-11-12 20:00:00 -0600

    displau owned NFTs in profile page

--- pages/people/[address].tsx
+++ pages/people/[address].tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { collection, doc, getDoc, getDocs, limit, query, where } from 'firebase/firestore'
 import { db } from '../../constants/firebase';
-import { IUser } from '../../types/types';
+import { IUser, IProduct } from '../../types/types';
+import Product from "../../components/Product";
 import PageLayout from '../../constants/PageLayout';
 import { AccountBox } from '../../pages/account';
+import { useAppContext } from '../../contexts/AppContext'
+
 
 interface IProps {
-  user: IUser
+  user: IUser,
+  productsArr: IProduct
 }
 
-const PublicProfile = ({ user }: IProps) => {
-  console.log(user);
+const PublicProfile = ({ user, productsArr }: any) => {
+  const appContext = useAppContext();
   
   return (
     <PageLayout>
@@ -19,14 +23,34 @@ const PublicProfile = ({ user }: IProps) => {
         twitter={user.twitter}
         address={user.address}
       />
+      <div className="flex flex-col lg:grid lg:grid-cols-2 lg:gap-12 w-6/12 mt-16 items-center fadeDown">
+        {productsArr.map((product: IProduct) => {
+          return (
+            <Product
+              buyNowPrice={product.buyNowPrice}
+              startingPrice={product.startingPrice}
+              title={product.title}
+              location={product.location}
+              description={product.description}
+              imagePath={product.imagePath}
+              refString={product.refString}
+              tokenURI={product.tokenURI}
+              listedSince={product.listedSince}
+              key={product.imagePath}
+              listedBy={''}
+              condition={''}
+              deliveryOpts={''}
+              ownerAddress={''}
+              ownerName={''}
+            />
+          )
+        })}
+      </div>
     </PageLayout>
   )
 }
 
 export async function getStaticProps({ params }: any) {
-  // const userRef = doc(db, 'products', `${params.address}`);
-  // const userRefValue = await getDoc(userRef);
-  // const user = userRefValue.data() as IUser;
   let user;
 
   const q = query(collection(db, 'users'), where('address','==',params.address));
@@ -40,9 +64,19 @@ export async function getStaticProps({ params }: any) {
     }
   })
 
+  const productsArr: Array<IProduct> = [];
+  const productsQuery = query(collection(db, 'products'), where('ownerAddress','==',params.address));
+  const productsDocs = await getDocs(productsQuery);
+
+  productsDocs.forEach((doc) => {
+    const product = doc.data();
+    productsArr.push(product as IProduct);
+  });
+  
   return {
     props: {
       user,
+      productsArr
     },
   }
 }
