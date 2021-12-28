75755d577172866b62600dfa7ddd4def67da6f40
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-12-28 03:01:46 -0600

    updated tailwind config to include nonstandard paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: 
+    ['./pages/**/*.{js,ts,jsx,tsx}', 
+    './components/**/*.{js,ts,jsx,tsx}',
+    './constants/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
