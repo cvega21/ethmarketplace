b3d53ef40ab70bc5b71c678174cfd4beb9bcbafc
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-12-28 03:07:34 -0600

    returning to deepLink function

--- components/ConnectMetamask.tsx
+++ components/ConnectMetamask.tsx
@@ -5,28 +5,23 @@ import { isMobile } from 'react-device-detect';
 const ConnectMetamask = () => {
   const appContext = useAppContext();
   const APP_DEEP_LINK = 'https://metamask.app.link/dapp/firecha.in';
+  
+  const mobileDeepLink = () => {
+    if (window.location.href !== APP_DEEP_LINK) {
+      location.replace(APP_DEEP_LINK);
+    }
 
-  if (isMobile) {
-    return (
-      <a href={APP_DEEP_LINK}>
-        <button  
-          className='bg-indigo-700 rounded-lg hover:bg-indigo-800 text-gray-100 hover:text-white font-light text-xl py-2 px-8 shadow-indigo w-full transition-all duration-200 ease-in-out'
-        >
-          <p>connect to metamask</p>
-        </button>
-      </a>
-    )   
-  } else {
-    return (
-      <button 
-        className='bg-indigo-700 rounded-lg hover:bg-indigo-800 text-gray-100 hover:text-white font-light text-xl py-2 px-8 shadow-indigo w-full transition-all duration-200 ease-in-out'
-        onClick={() => appContext?.connectMetamask()}
-      >
-        <p>connect to metamask</p>
-      </button>
-    )
+    appContext?.connectMetamask();
   }
- 
+
+  return (
+    <button 
+      className='bg-indigo-700 rounded-lg hover:bg-indigo-800 text-gray-100 hover:text-white font-light text-xl py-2 px-8 shadow-indigo w-full transition-all duration-200 ease-in-out'
+      onClick={isMobile ? () => mobileDeepLink() : () => appContext?.connectMetamask()}
+    >
+      <p>connect to metamask</p>
+    </button>
+  )
 }
 
 export default ConnectMetamask
