41b731d13a3aba93a30cbb421cee9b0c12521c6c
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-12-28 01:58:45 -0600

    trying a tag with href for deeplink

--- components/ConnectMetamask.tsx
+++ components/ConnectMetamask.tsx
@@ -4,19 +4,29 @@ import { isMobile } from 'react-device-detect';
 
 const ConnectMetamask = () => {
   const appContext = useAppContext();
-  
-  const mobileDeepLink = () => {
-    location.replace('https://metamask.app.link/dapp/firecha.in');
+  const APP_DEEP_LINK = 'https://metamask.app.link/dapp/firecha.in';
+
+  if (isMobile) {
+    return (
+      <a href={APP_DEEP_LINK}>
+        <button  
+          className='bg-indigo-700 rounded-lg hover:bg-indigo-800 text-gray-100 hover:text-white font-light text-xl py-2 px-8 shadow-indigo w-full transition-all duration-200 ease-in-out'
+        >
+          <p>connect to metamask</p>
+        </button>
+      </a>
+    )   
+  } else {
+    return (
+      <button 
+        className='bg-indigo-700 rounded-lg hover:bg-indigo-800 text-gray-100 hover:text-white font-light text-xl py-2 px-8 shadow-indigo w-full transition-all duration-200 ease-in-out'
+        onClick={() => appContext?.connectMetamask()}
+      >
+        <p>connect to metamask</p>
+      </button>
+    )
   }
  
-  return (
-    <button 
-      className='bg-indigo-700 rounded-lg hover:bg-indigo-800 text-gray-100 hover:text-white font-light text-xl py-2 px-8 shadow-indigo w-full transition-all duration-200 ease-in-out'
-      onClick={isMobile ? () => mobileDeepLink() : () => appContext?.connectMetamask()}
-    >
-      <p>connect to metamask</p>
-    </button>
-  )
 }
 
 export default ConnectMetamask
