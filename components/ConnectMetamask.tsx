bef122495f4d6c44512c8fcd5cbcfeb2c703f567
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-12-28 01:30:29 -0600

    added deeplink for mobile connect to metamask button

--- components/ConnectMetamask.tsx
+++ components/ConnectMetamask.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { useAppContext } from '../contexts/AppContext';
+import { isMobile } from 'react-device-detect';
 
 const ConnectMetamask = () => {
   const appContext = useAppContext();
-
+  
+  const mobileDeepLink = () => {
+    location.assign('https://metamask.app.link/dapp/firecha.in');
+  }
+ 
   return (
     <button 
       className='bg-indigo-700 rounded-lg hover:bg-indigo-800 text-gray-100 hover:text-white font-light text-xl py-2 px-8 shadow-indigo w-full transition-all duration-200 ease-in-out'
-      onClick={() => appContext?.connectMetamask()}
+      onClick={isMobile ? () => mobileDeepLink() : () => appContext?.connectMetamask()}
     >
       <p>connect to metamask</p>
     </button>
